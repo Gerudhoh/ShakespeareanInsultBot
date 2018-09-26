9f009202d8874bd17521dde81684796ed73c14ba
Julia Hohenadel <hohenade@uoguelph.ca>
2018-09-26 10:09:54 -0400

    Initial commit

---
+++ botCode.py
@@ -0,0 +1,7 @@
+import json
+import requests
+
+TOKEN = "620589232:AAFEV1A2V7rttFbVFHKJQLxvJBi5wxiUnL4"
+URL = "https://api.telegram.org/bot{}/".format(620589232:AAFEV1A2V7rttFbVFHKJQLxvJBi5wxiUnL4)
+
+
