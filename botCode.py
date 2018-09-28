bf295872f3075b760cd9e1146acd95bdfe4b19e0
Julia Hohenadel <hohenade@uoguelph.ca>
2018-09-27 21:05:14 -0400

    secret token got better

--- botCode.py
+++ botCode.py
@@ -1,15 +1,16 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 import json
 import requests
+import telegram
 
 f = open("tok.txt", "r")
 
-TOKEN = f.readline()
-
+TOKEN = f.readline().strip()
 f.close()
 
-URL = "https://api.telegram.org/bot{}/".format(TOKEN)
+#URL = "https://api.telegram.org/bot{}/".format(TOKEN)
 
-print ("token is: ", TOKEN)
+bot = telegram.Bot(token = TOKEN)
+print(bot.get_me())
 
